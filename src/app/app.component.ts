9a53af073e402ec7d530d89efe50cad7d722db40
DiegoHernandez <diego0930@gmail.com>
2024-08-23 15:44:38 -0500

    c2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { ListUserComponent } from './list-user/list-user.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CreateUserComponent,ListUserComponent],
+  imports: [RouterOutlet,CreateUserComponent,ListUserComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', redirectTo: '/Create', pathMatch: 'full' },
+    {path:'Create', component: CreateUserComponent },
+    {path:'Read', component: ListUserComponent },
+    {path:'Update', component: EditUserComponent },
+    {path:'Delete', component: DeleteUserComponent },
+];
